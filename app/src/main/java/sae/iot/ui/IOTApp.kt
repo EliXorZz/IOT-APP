2794cd0c502528481836048f60c5101731df18ec
Ethan Legros <ethan.legros@etu.univ-smb.fr>
2025-01-24 11:18:39 +0100

    add translations

--- app/src/main/java/sae/iot/ui/IOTApp.kt
+++ app/src/main/java/sae/iot/ui/IOTApp.kt
@@ -12,12 +12,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import sae.iot.R
 import sae.iot.ui.components.topbar.BottomBar
 import sae.iot.ui.components.topbar.TopBar
 import sae.iot.ui.screens.ActuatorScreen
@@ -42,9 +44,9 @@ fun IOTApp(
     navController: NavHostController = rememberNavController()
 ) {
     val items = listOf(
-        NavigationItem(title = "Home", icon = Icons.Outlined.Home, screen = IOTScreen.Home),
-        NavigationItem(title = "Actionneurs", icon = Icons.Outlined.PlayArrow, IOTScreen.Actions),
-        NavigationItem(title = "Paramètres", icon = Icons.Outlined.Settings, IOTScreen.Settings)
+        NavigationItem(title = LocalContext.current.getString(R.string.home), icon = Icons.Outlined.Home, screen = IOTScreen.Home),
+        NavigationItem(title = LocalContext.current.getString(R.string.actuator), icon = Icons.Outlined.PlayArrow, IOTScreen.Actions),
+        NavigationItem(title = LocalContext.current.getString(R.string.settings), icon = Icons.Outlined.Settings, IOTScreen.Settings)
     )
 
     val iotViewModel: HomeViewModel =
@@ -58,7 +60,7 @@ fun IOTApp(
             topBar = {
                 TopBar(
                     from = "IUT Annecy",
-                    alert = "Une alerte de fous !",
+                    alert = LocalContext.current.getString(R.string.alertDefault),
                     modifier = Modifier.padding(horizontal = 15.dp)
                 )
             },
